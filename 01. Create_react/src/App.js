d9d92da9a7bea8c4a3e3ee6b41688702813c70c3
Ayan Biswas <149355413+Ayanbiswas123@users.noreply.github.com>
2024-04-25 10:33:23 +0530

    Add files via upload

---
+++ 01. Create_react/src/App.js
@@ -0,0 +1,19 @@
+
+
+
+function App() {
+  return (
+    <h1>Hello World | to creat a project using react write in the console : npx create-react-app folder name then press enter key
+    <br></br>
+    and run the project in the live surver written in the console : npm run start
+    <br></br>
+    <br></br>
+    <br></br>
+    To creat a Project folder using Vite in web dev written in the colsole: npm create vite@latest then 
+    write the folder name then select "React" from the option then select Javascript from the list then folder created
+      
+          </h1>
+  );
+}
+
+export default App;
